53e46614118f28c8ff024ff3438043396676acb6
Yaghik Pandey <yaghik.pandey@gmail.com>
2025-07-07 17:47:59 +1000

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -50,5 +51,9 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    ModelTrainer = ModelTrainer()
+    score = ModelTrainer.initiate_model_trainer(train_arr,test_arr)
+    print("Returned R2 Score:", score)
 
